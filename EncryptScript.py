c68f238b1a595ff8178837cd2e50933a1858bd4e
Moore, Sam <smoore5@catworks.arizona.edu>
2015-09-21 23:32:25 -0700

    insert comments

--- EncryptScript.py
+++ EncryptScript.py
@@ -1,4 +1,10 @@
 import Encrypt
 
-Encrypt.decrypt(Encrypt2.hash_key('hello'), 'lpp.java')
+Encrypt.decrypt(Encrypt.hash_key('hello'), 'lpp.java')
 # password then name of file will go above
+
+#  if a directory use the following
+
+# for(dirpath, dirnames, filenames) in os.walk(dir_path):
+#     for file in filenames:
+#         Encrypt.encrypt(hashed_password, dirpath + "/" + file)
